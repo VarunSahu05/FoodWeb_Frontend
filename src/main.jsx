21f86a1f7ce1c3c80e9cb1cbdedc65b7ea7bbf74
VarunSahu05 <varunsahu909090@gmail.com>
2025-02-20 16:33:11 +0530

    save

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
+import { BrowseRouter } from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowseRouter>
 
   <StoreContextProvider>
   <App/>
   </StoreContextProvider>
-  </HashRouter>
+  </BrowseRouter>
 )
